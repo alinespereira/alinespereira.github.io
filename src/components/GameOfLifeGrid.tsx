ef5ff869ad2cef4d570ae739e82e7c1cbb18def8
Aline <line.asramos@gmail.com>
2021-05-08 01:30:31 -0300

    square grid

--- src/components/GameOfLifeGrid.tsx
+++ src/components/GameOfLifeGrid.tsx
@@ -1,4 +1,7 @@
 import { Component } from 'react'
+import * as d3 from 'd3'
+
+import './GameOfLifeGrid.scss'
 
 export interface GameOfLifeProps {
   gridMin?: number,
@@ -10,23 +13,56 @@ export enum CellStatus {
   Dead = 0
 }
 
+export interface Cell {
+  posX: number,
+  posY: number,
+  cellStatus: CellStatus
+}
+
 export interface GameOfLifeState {
   id?: string,
   gridX: number,
   gridY: number,
-  cells?: Array<Array<CellStatus>>
+  cells: Array<Array<Cell>>,
+  play?: boolean
 }
 
 export class GameOfLifeGrid extends Component<GameOfLifeState> {
+  
+  componentDidMount() {
+    const width = 0.6 * Math.min(window.screen.availWidth, window.screen.availHeight)
+    const rectWidth = Math.floor(width / this.props.gridX)
+    const rectHeight = rectWidth
+    const height = this.props.gridX * rectWidth
+
+    const svg = d3.select('#game-board')
+    svg
+      .attr('width', width)
+      .attr('height', height)
+
+    svg
+      .selectAll('*')
+      .remove()
+
+    const row = svg.selectAll(".row")
+      .data<Array<Cell>>(this.props.cells)
+      .enter().append("g")
+      .attr("class", "row")
+
+    row
+      .selectAll(".square")
+      .data(d => d)
+      .enter().append("rect")
+      .attr("class", d => `square ${d.cellStatus === CellStatus.Alive? "alive" : "dead"}`)
+      .attr("x", d => d.posX * rectWidth)
+      .attr("y", d => d.posY * rectHeight)
+      .attr("width", rectWidth)
+      .attr("height", rectHeight)
+  }
+
   render() {
     return (
-      <table>
-        {this.props.cells?.map(row =>
-          <tr>
-            {row.map(cell => <td>{cell}</td>)}
-          </tr>
-        )}
-      </table>
+      <svg id="game-board" />
     )
   }
 }
--- src/pages/post/GameOfLife.tsx
+++ src/pages/post/GameOfLife.tsx
@@ -2,21 +2,29 @@ import { ChangeEvent, Component } from 'react'
 import {
   GameOfLifeProps,
   GameOfLifeState,
+  Cell,
   CellStatus,
   GameOfLifeGrid
 } from '../../components/GameOfLifeGrid'
+import { simulate } from '../../helpers/GameOfLifeSimulator'
 import { v4 as uuidV4 } from 'uuid'
 
 import * as Foundation from 'react-foundation'
-import { Pause, Shuffle, Play, Trash } from '@styled-icons/foundation'
+import { Pause, Shuffle, Play, Next, Trash } from '@styled-icons/foundation'
 
 class GameOfLife extends Component<GameOfLifeProps, GameOfLifeState> {
+  intervalId?: number
+  
   constructor(props: GameOfLifeProps) {
     super(props)
     this.updateGridXSize = this.updateGridXSize.bind(this)
     this.updateGridYSize = this.updateGridYSize.bind(this)
-    this.updateGrid = this.updateGrid.bind(this)
-    this.updateGrid()
+    this.resetGrid = this.resetGrid.bind(this)
+    this.randomizeGrid = this.randomizeGrid.bind(this)
+    this.startSimulation = this.startSimulation.bind(this)
+    this.stepSimulation = this.stepSimulation.bind(this)
+    this.stopSimulation = this.stopSimulation.bind(this)
+    this.resetGrid()
   }
   
   static defaultProps = {
@@ -27,42 +35,84 @@ class GameOfLife extends Component<GameOfLifeProps, GameOfLifeState> {
   state = {
     gridX: 50,
     gridY: 50,
-    cells: undefined,
-    id: uuidV4()
+    cells: [],
+    id: uuidV4(),
+    play: false
   }
 
   componentDidMount() {
-    this.updateGrid()
+    this.resetGrid()
   }
 
-  updateGrid() {
-    this.setState({ cells: undefined })
+  resetGrid() {
+    const cells: Array<Array<Cell>> = []
 
-    console.log(this.state.gridX)
-    console.log(this.state.gridY)
-    
-    const cells: Array<Array<CellStatus>> =
-      Array(this.state.gridX)
-        .fill(Array(this.state.gridY).fill(CellStatus.Dead))
-    console.log(cells)
+    for (let posX = 0; posX < this.state.gridX; posX++) {
+      cells[posX] = []
+      for (let posY = 0; posY < this.state.gridX; posY++) {
+        cells[posX][posY] = { posX, posY, cellStatus: CellStatus.Dead }
+      }
+    }
     this.setState({
       cells,
       id: uuidV4()
     })
   }
 
+  randomizeGrid() {
+    const aliveProbability = 0.3
+    const cells: Array<Array<Cell>> = []
+
+    for (let posX = 0; posX < this.state.gridX; posX++) {
+      cells[posX] = []
+      for (let posY = 0; posY < this.state.gridX; posY++) {
+        cells[posX][posY] = {
+          posX,
+          posY,
+          cellStatus: Math.random() < aliveProbability ? CellStatus.Alive : CellStatus.Dead }
+      }
+    }
+    this.setState({
+      cells,
+      id: uuidV4()
+    })
+  }
+
+  startSimulation() {
+    if (!this.state.play && !this.intervalId)
+      this.intervalId = window.setInterval(this.stepSimulation, 500)
+    this.setState({
+      play: true,
+      id: uuidV4()
+    })
+  }
+
+  stepSimulation() {
+    const cells = simulate(this.state.cells, this.state.gridX, this.state.gridY)
+    this.setState({
+      cells,
+      id: uuidV4()
+    })
+  }
+
+  stopSimulation() {
+    window.clearInterval(this.intervalId)
+    this.intervalId = undefined
+    this.setState({ play: false, id: uuidV4() })
+  }
+
   updateGridXSize(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ gridX: parseInt(event.target.value) }, this.updateGrid)
+    this.setState({ gridX: parseInt(event.target.value) }, this.resetGrid)
   }
 
   updateGridYSize(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ gridY: parseInt(event.target.value) }, this.updateGrid)
+    this.setState({ gridY: parseInt(event.target.value) }, this.resetGrid)
   }
   
   render() {
     const {
       gridX,
-      gridY
+      // gridY
     } = this.state
 
     const {
@@ -86,9 +136,10 @@ class GameOfLife extends Component<GameOfLifeProps, GameOfLifeState> {
             />
           </Foundation.Cell>
           <Foundation.Cell small={6} medium={4} large={2}>
-            <input type="number" id="vertical-grid-size"
+            <input className="hide" type="number" id="vertical-grid-size"
+              disabled
               min={gridMin} max={gridMax}
-              value={gridY}
+              //value={gridY}
               onChange={this.updateGridYSize}
             />
           </Foundation.Cell>
@@ -98,29 +149,43 @@ class GameOfLife extends Component<GameOfLifeProps, GameOfLifeState> {
           <Foundation.Cell small={12} medium={6} large={4}>
             <Foundation.Menu alignment={Foundation.Alignments.CENTER} isExpanded>
               <Foundation.MenuItem>
-                <Trash size={32} />
+                <Trash size={32}
+                  onClick={this.resetGrid}
+                />
               </Foundation.MenuItem>
               <Foundation.MenuItem>
-                <Shuffle size={32} />
+                <Shuffle size={32}
+                  onClick={this.randomizeGrid}
+                />
               </Foundation.MenuItem>
               <Foundation.MenuItem>
-                <Play size={32} />
+                <Play size={32} 
+                  onClick={this.startSimulation}
+                />
               </Foundation.MenuItem>
               <Foundation.MenuItem>
-                <Pause size={32} />
+                <Next size={32}
+                  onClick={this.stepSimulation}
+                />
+              </Foundation.MenuItem>
+              <Foundation.MenuItem>
+                <Pause size={32} 
+                  onClick={this.stopSimulation}
+                />
               </Foundation.MenuItem>
             </Foundation.Menu>
           </Foundation.Cell>
         </Foundation.Grid>
 
         <Foundation.Grid>
-          <Foundation.Cell small={12}>
-            <div id="game-board">
+          <Foundation.Cell small={12} className="grid-padding-y">
+            <div className="text-center" id="game">
               <GameOfLifeGrid
                 key={this.state.id}
                 gridX={this.state.gridX}
                 gridY={this.state.gridY}
                 cells={this.state.cells}
+                play={this.state.play}
               />
             </div>
           </Foundation.Cell>
